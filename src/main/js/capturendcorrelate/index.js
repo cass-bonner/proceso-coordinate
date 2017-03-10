0fff6b8d15ad6a0fa17111659961bdea766bb2ae
Bonner, Cassandra <cass_bonner@yahoo.com.au>
2017-03-10 18:18:48 +1100

    forcing rebuild

--- src/main/js/capturendcorrelate/index.js
+++ src/main/js/capturendcorrelate/index.js
@@ -1,6 +1,8 @@
 var common= require('common');
 exports.handler = (event, context, callback) => {
 
+    // perform any setup - here prepending correlationId in logs.
+    var correlationId=common.initCorrelation();
    // TODO - ideally we won't pass in the public facing url, but does make it easier to display later anyway.
    var s3Path = event.resourcefile;
    var type = event.type;
@@ -18,14 +20,14 @@ exports.handler = (event, context, callback) => {
     console.log('filename:',filename);
 
     
-    // perform any setup - here prepending correlationId in logs.
-    var correlationId=common.initCorrelation();
     var response={};
     response["correlationId"]=correlationId;
     response["s3Path"]=s3Path;
     response["type"]=type;
     response["hashtag"]=hashtag;
     response["processingInstructions"]=processingInstructions;
+    response["filename"]=filename;
+    response["bucket"]=bucket;
     
     console.log('returning',response);
     callback(null, response);
